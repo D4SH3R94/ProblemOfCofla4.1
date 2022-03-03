d8a66c46ce0e0f8539d352e1a5a5bfd520a056c7
miguelpaulino239@gmail.com <miguelpaulino239.com>
2022-03-03 17:38:23 -0500

    funciability of opcions 1 ready

--- programOfSchool.js
+++ programOfSchool.js
@@ -3,16 +3,34 @@ let programacion = ["Jesler","Yordi","Mario","Fernando"];
 let quimica = ["Miguel","Lixandra","Mario","Fernando"];
 let logica = ["Daniela","Aliet","Miguel"];
 let profesores = ["Osvaldo","Rafael","Pocho","Alain"];
+let materias = [fisica,programacion,quimica,logica];
+let testArrays = ["fisica","programacion","quimica","logica"];
 
 const obtentionInfo = (signature)=>{
-    let materias = [fisica,programacion,quimica,logica];
-    
-    if(materias.includes(signature) == true){
-        let poscitionMater = materias.indexOf(signature);
+    if(testArrays.includes(signature) == true){
+        let poscitionMater = testArrays.indexOf(signature);
         document.write(`<b>Profesor:</b> ${profesores[poscitionMater]}<br><b>Alumnos:</b> ${materias[poscitionMater]}<br>`);
     }else{
         alert("Error, por favor escriba una asignatura correcta.")
     }
 }
-
 obtentionInfo(quimica);
+
+const funcionalArrays = (pSignature) =>{
+    let materia1 = [fisica.push(pSignature),programacion.push(pSignature),quimica.push(pSignature),logica.push(pSignature)];
+}
+
+const opcionsProgramOfSchool = () =>{
+    let opcionsP = prompt("Marque 1-Para obtener informacion de Clases,2-Para saber informacion personal de clases,3-Para saber Informacion docente.")    
+    if(opcionsP == 1){
+        let questSignature = prompt("Diga la Clase.");
+        let testArrays = ["fisica","programacion","quimica","logica"]
+        console.log(testArrays);
+        console.log(testArrays.includes(questSignature));
+        if(testArrays.includes(questSignature) == true ){
+            obtentionInfo(questSignature);
+        }
+    }
+}
+
+opcionsProgramOfSchool();
